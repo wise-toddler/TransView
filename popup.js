90f8321c89aa1ca44d2f6030a02b5d6c3e0e17a4
Shivansh SIngh <wiseeldrich2004@gmail.com>
2023-11-04 19:20:26 +0530

    sachiin last hope

--- popup.js
+++ popup.js
@@ -1,24 +1,37 @@
-// Function to update the transparency based on the slider value
-function updateTransparency(value) {
-    const opacity = value / 100;
-    const iframe = document.getElementById('webContent');
-    iframe.style.opacity = opacity;
-    document.getElementById('transparencyValue').textContent = value + '%';
-}
 
-// Function to load the webpage from the active tab
-function loadActiveTabWebpage() {
-    getActiveTabUrl(function(url) {
-        const iframe = document.getElementById('webContent');
-        iframe.src = url;
+// Function to open the current page's link in a popup window
+function openLinkInPopup() {
+    // Get the current active tab's URL
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentUrl = tabs[0].url;
+        // Set the URL of the popup window to the current page's URL
+        window.open(currentUrl, 'NewPopup', 'width=800,height=600,resizable=yes,scrollbars=yes');
     });
 }
 
+// Function to update the transparency of the web content based on the slider value
+function updateTransparency(value, popupWindow) {
+    const opacity = value / 100;
+    const iframe = popupWindow.document.getElementById('webContent');
+    const webContent = iframe.contentDocument.body; // Access the content inside the iframe
+    iframe.style.opacity = opacity;
+    webContent.style.opacity = opacity; // Set opacity for the content inside the iframe
+    popupWindow.document.getElementById('transparencyValue').textContent = value + '%';
+}
 // Add an event listener for the transparency slider
 const slider = document.getElementById('transparencySlider');
 slider.addEventListener('input', function () {
     updateTransparency(this.value);
 });
 
-// Load the webpage from the active tab when the extension is clicked
-loadActiveTabWebpage();
+// // Add an event listener to load the webpage from the active tab when the extension is clicked
+// loadActiveTabWebpage();
+
+// Add an event listener for the "Open New Popup" button
+const openNewPopupButton = document.getElementById('openNewPopupButton');
+openNewPopupButton.addEventListener('click', openLinkInPopup);
+// let t =0;
+// if(t==0){
+//     openLinkInPopup();
+//     t++;
+// }
