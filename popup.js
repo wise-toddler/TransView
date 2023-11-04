c6638fe5e23a42bf90162b0c2cef8978de5aeb1a
Shivansh SIngh <wiseeldrich2004@gmail.com>
2023-11-04 13:48:33 +0530

    doenee

--- popup.js
+++ popup.js
@@ -1,45 +1,24 @@
-// In popup.js
-
-// Function to adjust transparency based on the slider value
-function adjustTransparency() 
-{
-    const slider = document.getElementById('transparencySlider');
-    const transparencyValue = document.getElementById('transparencyValue');
-    const webContentDiv = document.getElementById('webContent');
-
-    // Get the slider value and update transparency
-    const value = slider.value;
+// Function to update the transparency based on the slider value
+function updateTransparency(value) {
     const opacity = value / 100;
-    transparencyValue.innerText = value + '%';
-
-    // Apply transparency to the web content
-    webContentDiv.style.opacity = opacity;
+    const iframe = document.getElementById('webContent');
+    iframe.style.opacity = opacity;
+    document.getElementById('transparencyValue').textContent = value + '%';
 }
 
-// Function to load web content into the "webContent" div
-function loadWebContent(url) 
-{
-    const webContentDiv = document.getElementById('webContent');
-
-    // Create an iframe element to display the website content
-    const iframe = document.createElement('iframe');
-    iframe.src = url;
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none'; // Remove iframe border
-    iframe.setAttribute('allow', 'geolocation'); // Optional: Allow geolocation access
-
-    // Append the iframe to the "webContent" div
-    webContentDiv.appendChild(iframe);
+// Function to load the webpage from the active tab
+function loadActiveTabWebpage() {
+    getActiveTabUrl(function(url) {
+        const iframe = document.getElementById('webContent');
+        iframe.src = url;
+    });
 }
 
-// Add an event listener for the slider to adjust transparency
+// Add an event listener for the transparency slider
 const slider = document.getElementById('transparencySlider');
-slider.addEventListener('input', adjustTransparency);
+slider.addEventListener('input', function () {
+    updateTransparency(this.value);
+});
 
-// Example URL (replace with active URL)
-const websiteURL = 'https://www.google.com';
-
-// Load the web content and set an initial transparency value
-loadWebContent(websiteURL);
-adjustTransparency(); // Set initial transparency based on the slider value
+// Load the webpage from the active tab when the extension is clicked
+loadActiveTabWebpage();
